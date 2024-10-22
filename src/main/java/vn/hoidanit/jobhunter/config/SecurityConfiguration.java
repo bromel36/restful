1fc500344a83b5b88238a347e3979236c879dd5b
Nguyễn Minh Tiến <nguyentien7a8@gmail.com>
2024-10-22 17:54:45 +0700

    update

--- src/main/java/vn/hoidanit/jobhunter/config/SecurityConfiguration.java
+++ src/main/java/vn/hoidanit/jobhunter/config/SecurityConfiguration.java
@@ -15,6 +15,8 @@ import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+import org.springframework.security.oauth2.server.resource.web.BearerTokenAuthenticationEntryPoint;
+import org.springframework.security.oauth2.server.resource.web.access.BearerTokenAccessDeniedHandler;
 import org.springframework.security.web.SecurityFilterChain;
 import vn.hoidanit.jobhunter.util.SecurityUtil;
 
@@ -42,9 +44,15 @@ public class SecurityConfiguration {
                                 .anyRequest().authenticated()
 //                                .anyRequest().permitAll()
                 )
+                .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()))
+                .exceptionHandling(
+                        exceptions -> exceptions
+                                .authenticationEntryPoint(new BearerTokenAuthenticationEntryPoint()) //401
+                                .accessDeniedHandler(new BearerTokenAccessDeniedHandler())) //403
+
                 .formLogin(form -> form.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
+        ;
         return http.build();
     }
 
--- src/main/java/vn/hoidanit/jobhunter/util/error/GlobalException.java
+++ src/main/java/vn/hoidanit/jobhunter/util/error/GlobalException.java
@@ -2,6 +2,8 @@ package vn.hoidanit.jobhunter.util.error;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -14,6 +16,28 @@ import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalException {
+    /*
+    * in function loadUserByUsername() which we override in UserDetailsCustom.java, when can't find user by username, we throw
+    * UsernameNotFoundException, then, spring security will catch and finally, it throws BadCredentialsException
+    * so in this function, it always catches BadCredentialsException
+    * but in password is incorrect, spring security will finally throw BadCredentialsException, and we
+    * not be done anything
+    *
+    * the purpose of function is show error when login failed
+    * */
+    @ExceptionHandler(value = {
+            UsernameNotFoundException.class,
+            BadCredentialsException.class,
+    })
+    public ResponseEntity<RestResponse<Object>> handleUsernameNotFoundException(BadCredentialsException ex){
+        RestResponse<Object> res = new RestResponse<>();
+        res.setMessage("Exception occur ....");
+        res.setError(ex.getMessage());
+        res.setStatusCode(HttpStatus.BAD_REQUEST.value());
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(res);
+    }
+
     @ExceptionHandler(IdInvalidException.class)
     public ResponseEntity<RestResponse<Object>> handleIdInvalidException(IdInvalidException ex){
         RestResponse<Object> res = new RestResponse<>();
@@ -25,7 +49,7 @@ public class GlobalException {
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<RestResponse<Object>> handleIdInvalidException(MethodArgumentNotValidException ex){
+    public ResponseEntity<RestResponse<Object>> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex){
         RestResponse<Object> res = new RestResponse<>();
         BindingResult bindingResult = ex.getBindingResult();
         List<FieldError> fieldErrors = bindingResult.getFieldErrors();
